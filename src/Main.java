6cd3ccfa256df61ffd613242a075fc1b407dc875
ksnksn <ksnksn2@yandex.ru>
2017-05-20 07:43:50 +0300

    master 1111

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ ksnksnksn33
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello World! 1111");
     }
 }
